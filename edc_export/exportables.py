79050dcffb5e8c08490c6cb9289135e0ecd2ec94
erikvw <ew2789@gmail.com>
2019-01-12 17:43:11 -0600

    reduce complexity

--- edc_export/exportables.py
+++ edc_export/exportables.py
@@ -1,12 +1,11 @@
 from collections import OrderedDict
 from django.apps import apps as django_apps
 from django.contrib import messages
+from django.contrib.admin import sites
 from django.core.exceptions import ObjectDoesNotExist
 from edc_permissions.constants.group_names import EXPORT
 
 from .model_options import ModelOptions
-from django.contrib.admin.sites import site
-from django.contrib.admin import sites
 
 
 class Exportables(OrderedDict):
@@ -22,21 +21,9 @@ class Exportables(OrderedDict):
 
     def __init__(self, app_configs=None, user=None, request=None):
         super().__init__()
+        self._inlines = {}
         app_configs = app_configs or self.get_app_configs()
         app_configs.sort(key=lambda x: x.verbose_name)
-
-        inlines = {}
-        for site in sites.all_sites:
-            for model_cls, admin_site in site._registry.items():
-                for inline_cls in admin_site.inlines:
-                    model_opts = ModelOptions(
-                        model=inline_cls.model._meta.label_lower)
-                    try:
-                        inlines[model_cls._meta.app_label].append(model_opts)
-                    except KeyError:
-                        inlines[model_cls._meta.app_label] = [model_opts]
-                    inlines[model_cls._meta.app_label].sort(
-                        key=lambda x: x.verbose_name.title())
 
         try:
             user.groups.get(name=self.export_group_name)
@@ -61,10 +48,28 @@ class Exportables(OrderedDict):
                 list_models.sort(key=lambda x: x.verbose_name.title())
                 exportable = {
                     'models': models,
-                    'inlines': inlines.get(model._meta.app_label),
+                    'inlines': self.inlines.get(model._meta.app_label),
                     'historicals': historical_models,
                     'lists': list_models}
                 self.update({app_config: exportable})
+
+    @property
+    def inlines(self):
+        if not self._inlines:
+            for site in sites.all_sites:
+                for model_cls, admin_site in site._registry.items():
+                    for inline_cls in admin_site.inlines:
+                        model_opts = ModelOptions(
+                            model=inline_cls.model._meta.label_lower)
+                        try:
+                            self._inlines[model_cls._meta.app_label].append(
+                                model_opts)
+                        except KeyError:
+                            self._inlines[model_cls._meta.app_label] = [
+                                model_opts]
+                        self._inlines[model_cls._meta.app_label].sort(
+                            key=lambda x: x.verbose_name.title())
+        return self._inlines
 
     def get_app_configs(self):
         """Returns a list of app_configs with exportable data.
