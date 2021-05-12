e6cbf8b41211cdd2856463d4c7496a9a36cee9eb
erikvw <ew2789@gmail.com>
2021-05-11 19:51:50 -0500

    update with edc admin site

--- edc_export/admin_site.py
+++ edc_export/admin_site.py
@@ -1,11 +1,5 @@
 from edc_model_admin.admin_site import EdcAdminSite
 
+from .apps import AppConfig
 
-class AdminSite(EdcAdminSite):
-    site_header = "Edc Export"
-    site_title = "Edc Export"
-    index_title = "Edc Export Administration"
-    site_url = "/"
-
-
-edc_export_admin = AdminSite(name="edc_export_admin")
+edc_export_admin = EdcAdminSite(name="edc_export_admin", app_label=AppConfig.name)
